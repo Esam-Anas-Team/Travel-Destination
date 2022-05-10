453e063020c0d9337b5e4594652974c344b7f992
abuobaid9 <abuobaid.anas1999@gmail.com>
2022-05-10 21:47:04 +0300

    add footer

---
+++ src/components/home/Home.js
@@ -0,0 +1,21 @@
+import './Home.css';
+import '../footer/Footer';
+import '../header/Header';
+import '../tours/Tours';
+import Header from '../header/Header';
+import Tours from '../tours/Tours';
+import Footer from '../footer/Footer';
+function Home(props) {
+
+
+    return (
+        <>
+        <Header/>
+        {/* <Tours /> */}
+        <Footer />
+          
+        </>
+    )
+}
+
+export default Home;
